6058117bb71369a1b762b67a2caa7c33b8b522ef
omerdr7 <omerfarukdere32@gmail.com>
2024-06-13 20:10:32 +0300

    areFriends method created with for loop and if statement to find friend number.

--- src/friendnumbers/FriendNumbers.java
+++ src/friendnumbers/FriendNumbers.java
@@ -2,9 +2,28 @@ package friendnumbers;
 
 public class FriendNumbers {
 
+    public static boolean areFriens(int x, int y) {
+        boolean control = false;
+        int sumOfX = 0;
+        int sumOfY = 0;
+        for (int i = 1; i < x; i++) {
+            if (x % i == 0) {
+                sumOfX += i;
+            }
+        }
+        for (int i = 1; i < y; i++) {
+            if (y % i == 0) {
+                sumOfY += i;
+            }
+        }
+        if (sumOfX == y && sumOfY == x) {
+            control = true;
+        }
+        return control;
+    }
+
     public static void main(String[] args) {
-        
-        
+
     }
 
 }
